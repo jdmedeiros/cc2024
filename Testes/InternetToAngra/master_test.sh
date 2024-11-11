0a6cf5f4c36942a9079900294e4bf6bfbfe52b83
Dan Medeiros <jdmedeiros@gmail.com>
2024-11-11 05:03:15 +0000

    Adds tests

--- Testes/InternetToAngra/master_test.sh
+++ Testes/InternetToAngra/master_test.sh
@@ -10,7 +10,7 @@ read -rsn1 -p"Ready? - press any key to continue";echo
 echo -e "\nStarting SSH tests..."
 ./ssh_test.sh
 
-read -rsn1 -p"Ready? - press any key to continue";echo;echo
+read -rsn1 -p"Ready? - press any key to continue";echo
 
 echo -e "\nStarting HTTP/HTTPS tests..."
 ./http_https_test.sh
